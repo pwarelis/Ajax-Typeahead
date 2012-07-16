2487e2912eb032a0032255fdd20627f469def4ae
Bernard Chhun <bernard.chhun@gmail.com>
2012-07-16 12:24:48 -0300

    Corrected a small typo error before the item variable

--- typeahead.js
+++ typeahead.js
@@ -202,7 +202,7 @@
 		},
 	
 		matcher: function (item) {
-			return ~item.toLowerCase().indexOf(this.query.toLowerCase())
+			return item.toLowerCase().indexOf(this.query.toLowerCase())
 		},
 
 		sorter: function (items) {
